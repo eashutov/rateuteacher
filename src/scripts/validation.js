0f62b87f7bcdcfb0dbe782974c38eddfdf5c1aa8
Egor Shutov <3g0rk44z@gmail.com>
2023-03-23 00:53:59 +0600

    rateuteacher v1

---
+++ src/scripts/validation.js
@@ -0,0 +1,27 @@
+var radio = document.querySelectorAll('input[type="radio"]');
+var valid = document.querySelector('input[type="submit"]');
+var reset = document.querySelector('input[type="reset"]');
+
+document.body.addEventListener('change', () => {
+    for(var i = 0; i < radio.length; i += 5) {
+        for(var j = 0; j < 5; j++) {
+            if(!radio[i+j].checked) {
+                valid.disabled = true;
+            } else {
+                valid.disabled = false;
+                break;
+            }
+        }
+        if(valid.disabled) {
+            return;
+        } 
+    }
+    document.getElementById('submit-span').style.opacity = 0;
+    console.log('ready to submit');
+});
+
+reset.addEventListener('click', () => {
+    valid.disabled = true;
+    document.getElementById('submit-span').style.opacity = 1;
+});
+
