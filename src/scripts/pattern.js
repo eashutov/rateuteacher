a7d6798cc558fbcbd0115029efaf01fb6424fae5
Egor Shutov <3g0rk44z@gmail.com>
2023-04-22 15:45:55 +0600

    changed pattern from 3 to 4 integers

--- src/scripts/pattern.js
+++ src/scripts/pattern.js
@@ -1,6 +1,6 @@
 var codeButton = document.getElementById('btn');
 var input = document.getElementById('code');
-const re = new RegExp('^[А-Яа-яЁё]{2,5}-[0-9]{3}-[0-9]{1,3}-[0-9]{3}$');
+const re = new RegExp('^[А-Яа-яЁё]{2,5}-[0-9]{3}-[0-9]{1,3}-[0-9]{4}$');
 
 codeButton.disabled = true;
 
