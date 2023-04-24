1ca50c15824a54f8f4eeccb1bb6dd8d45d972bb2
Egor Shutov <3g0rk44z@gmail.com>
2023-04-24 22:03:08 +0600

    added header and footer templates

--- profile.php
+++ profile.php
@@ -21,37 +21,7 @@
 <body>
     <div class="wrapper">
 
-
-        <!-- HEADER -->
-
-
-        <header class="site-header">
-            <div class="site-search">
-                <form action="" method="GET" class="search-bar">
-                    <input id="search" type="search" placeholder="Поиск по сайту" name="q" autocomplete="off">
-                    <button type="submit"><img src="src/images/search.svg" alt="x" width="20px" height="20px"></button>
-                </form>
-            </div>
-            <div class="site-logo">
-                <a href="index.html"><img src="src/images/rut_new.png" alt="rut" width="60px" height="60px"/></a>
-            </div>
-            <nav class="site-navigation">
-                <ul class="nav">
-                    <li class="hover"><a href="#">Рейтинги</a></li>
-                    <li class="hover"><a href="#">Контакты</a></li>
-                    <li class="hover"><a href="#">Q&ampA</a></li>
-                </ul>
-            </nav>
-            <div class="site-sign">
-                <div class="hover">
-                    <a href="#" class="sign"><img src="src/images/sign.svg" alt="sign" width="20px" height="20px" /></a>
-                </div>
-            </div>
-        </header>
-        
-
-        <!-- MAIN -->
-
+        <?php include("src/app/header.php"); ?>
 
         <main class="main-grid-type">
             <div></div>
@@ -173,25 +143,7 @@
             </div>
         </main>
 
-
-        <!-- FOOTER -->
-
-
-        <footer class="site-footer">
-            <div class="dev-contacts">
-                <p>8-800-555-35-35</p>
-                <p>egorkamaxwell@yandex.ru</p>
-            </div>
-            <div class="powered-by">
-                <h2>OmSTU, 2023</h2>
-                <p>by E.A. Shutov</p>
-            </div>
-            <div class="references hover">
-                <a href="#"><img src="src/images/discord_logo.svg" alt="DS"></a>
-                <a href="https://omgtu.ru/"><img src="src/images/omstu-logo.svg" alt="OmSTU"></a>
-                <a href="#"><img src="src/images/vk_logo.svg" alt="VK"></a>
-            </div>
-        </footer>
+        <?php include("src/app/footer.php") ?>
     
     </div>
 </body>
