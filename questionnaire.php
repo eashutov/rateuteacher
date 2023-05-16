05f7cc8f215b430ed509ff448d6fafac712fb0bd
Egor Shutov <3g0rk44z@gmail.com>
2023-05-16 15:38:28 +0600

    added head template

--- questionnaire.php
+++ questionnaire.php
@@ -15,11 +15,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include("src/app/head.php"); ?>
     <title>RateUTeacher</title>
-    <link rel="stylesheet" href="src/styles/style.css">
     <script src="src/scripts/modal.js" defer></script>
     <script src="src/scripts/btnup.js" defer></script>
     <script src="src/scripts/validation.js" defer></script>
